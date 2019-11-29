6a365946407f81eac56e07ec7be7a13c3cf15ed1
SimretA <simretaraya7@gmail.com>
2019-11-29 06:01:50 +0300

    day 6 I

---
+++ D6/matrix_cell.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def allCellsDistOrder(self, R, C, r0, c0):
+        """
+        :type R: int
+        :type C: int
+        :type r0: int
+        :type c0: int
+        :rtype: List[List[int]]
+        """
--- D6/relative_arr_sort.py
+++ D6/relative_arr_sort.py
@@ -1,7 +1,23 @@
-def relativeSortArray(arr1, arr2):
-    """
-    :type arr1: List[int]
-    :type arr2: List[int]
-    :rtype: List[int]
-    """
-    
+class Solution(object):
+    def relativeSortArray(self, arr1, arr2):
+        """
+        :type arr1: List[int]
+        :type arr2: List[int]
+        :rtype: List[int]
+        """
+        result = list()
+        for i in arr2:
+            count = 0
+            while len(arr1) > count:
+                if i == arr1[count]:
+                    result.append(arr1.pop(count))
+                    continue
+                count += 1
+        if len(arr1) > 0:
+            result += sorted(arr1)
+        return result
+
+
+if __name__ == '__main__':
+    c = Solution()
+    print(c.relativeSortArray([28, 6, 22, 8, 44, 17], [22, 28, 8, 6]))
